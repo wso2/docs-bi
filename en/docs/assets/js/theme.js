c4ae5c64c9bb8120048b407567aa834b3fc15fb6
Sachindu-Nethmin <sachindunethminweerasinghe@gmail.com>
2026-01-26 11:24:42 +0530

    Fix loop variable declaration in theme.js and correct copyright year in 404.html

--- en/docs/assets/js/theme.js
+++ en/docs/assets/js/theme.js
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   // Open external links in new tab
   var links = document.links;
-  for (let j = 0, linksLength = links.length; i < linksLength; i++) {
+  for (let i = 0, linksLength = links.length; i < linksLength; i++) {
     if (links[i].hostname != window.location.hostname) {
       links[i].target = "_blank";
       links[i].setAttribute("rel", "noopener noreferrer");
